bc4e84ec77eb7b1d02ef6fddecb45954292073dd
wdw000 <dowonwang01@gmail.com>
2022-11-19 00:10:44 +0900

    [modified] 연속된 수의 합 수정

--- programmers/src/lv.0/연속된 수의 합/연속된_수의_합.js
+++ programmers/src/lv.0/연속된 수의 합/연속된_수의_합.js
@@ -1,7 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function solution(num, total) {
-  const temp = total % num;
-  return temp;
+  const result = Array(num);
+  const idx = Math.ceil((num - 1) / 2);
+  let temp = Math.ceil(total / num);
+
+  for (let i = 0; i < result.length; i++) {
+    if (i < idx) {
+      result[i] = temp - idx + i;
+    } else if (i === idx) {
+      result[i] = temp;
+    } else {
+      result[i] = temp + i - idx;
+    }
+  }
+  return result;
 }
-console.log(solution(4, 16));
--- programmers/src/lv.0/연속된 수의 합/연속된_수의_합.ts
+++ programmers/src/lv.0/연속된 수의 합/연속된_수의_합.ts
@@ -1,17 +1,43 @@
+/*
+연속된 세 개의 정수를 더해 12가 되는 경우는 3, 4, 5입니다. 두 정수 num과 total이 주어집니다. 연속된 수 num개를 더한 값이 total이 될 때, 정수 배열을 오름차순으로 담아 return하도록 solution함수를 완성해보세요.
+
+제한사항
+1 ≤ num ≤ 100
+0 ≤ total ≤ 1000
+num개의 연속된 수를 더하여 total이 될 수 없는 테스트 케이스는 없습니다.
+
+입출력 예 설명
+입출력 예 #1
+
+num = 3, total = 12인 경우 [3, 4, 5]를 return합니다.
+입출력 예 #2
+
+num = 5, total = 15인 경우 [1, 2, 3, 4, 5]를 return합니다.
+입출력 예 #3
+
+4개의 연속된 수를 더해 14가 되는 경우는 2, 3, 4, 5입니다.
+입출력 예 #4
+
+설명 생략
+*/
+
 function solution(num: number, total: number) {
   const result = Array<number>(num);
 
-  if (total % num === 0) {
-    const temp = (num - 1) / 2;
+  const idx = Math.ceil((num - 1) / 2);
+  let temp = Math.ceil(total / num);
 
-    for (let i = 0; i < result.length; i++) {}
-  } else {
-    let temp = num;
+  for (let i = 0; i < result.length; i++) {
+    if (i < idx) {
+      result[i] = temp - idx + i;
+    } else if (i === idx) {
+      result[i] = temp;
+    } else {
+      result[i] = temp + i - idx;
+    }
   }
 
   return result;
 }
-
-console.log(solution(3, 12));
 
 export {};
